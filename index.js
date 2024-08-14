c637f3a7be0113ca3f4cd73e1f280759bc0111e7
sandeepvarma1010 <103293241+sandeepvarma1010@users.noreply.github.com>
2024-08-13 20:02:27 -0700

    Day-1 Commit

--- index.js
+++ index.js
@@ -6,10 +6,17 @@ const jwt = require('jsonwebtoken');
 const User = require('./models/User');
 const Transaction = require('./models/Transaction');
 const authMiddleware = require('./authMiddleware');
+const cors = require('cors');
 
 const app = express();
+
+// Connect to the database
 connectDB();
 
+// Enable CORS (should be done before any routes or middleware that handle requests)
+app.use(cors());
+
+// Middleware to parse incoming JSON requests
 app.use(express.json());
 
 // User registration
@@ -101,6 +108,52 @@ app.get('/transactions', authMiddleware, async (req, res) => {
     try {
         const transactions = await Transaction.find({ user: req.user.id });
         res.json(transactions);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
+// Delete a transaction (protected)
+app.delete('/transactions/:id', authMiddleware, async (req, res) => {
+    try {
+        const transaction = await Transaction.findById(req.params.id);
+        if (!transaction) {
+            return res.status(404).json({ msg: 'Transaction not found' });
+        }
+
+        if (transaction.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'User not authorized' });
+        }
+
+        await transaction.deleteOne();  // Use deleteOne instead of remove
+        res.json({ msg: 'Transaction removed' });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
+// Update a transaction (protected)
+app.put('/transactions/:id', authMiddleware, async (req, res) => {
+    const { amount, type, category } = req.body;
+
+    try {
+        let transaction = await Transaction.findById(req.params.id);
+        if (!transaction) {
+            return res.status(404).json({ msg: 'Transaction not found' });
+        }
+
+        if (transaction.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'User not authorized' });
+        }
+
+        transaction.amount = amount || transaction.amount;
+        transaction.type = type || transaction.type;
+        transaction.category = category || transaction.category;
+
+        await transaction.save();
+        res.json(transaction);
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server error');
